fa52368e7a357dd3a2664ad637c1f0e12d7f264c
alexanderpttt <alexanderpttt@gmail.com>
2015-11-10 11:28:09 -0600

    add several gulp example: define,copy

--- copyFiles.js
+++ copyFiles.js
@@ -7,3 +7,8 @@ var gulp=require('gulp');
 gulp.task('copyHtml',function(){
     return gulp.src('./app/index.html').pipe(gulp.dest('dist'));
 });
+
+gulp.task('copyImage',function(){
+    //** 匹配任何字符 包括路径分隔符/   *号不能匹配路径分隔符/
+    return gulp.src('./app/images/**/*.{jpg,png}').pipe(gulp.dest('dist/images'));
+});
